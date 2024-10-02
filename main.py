57fc20d6a2e22ffc5675d08da7b307860ac2b3de
Thailer423 <danielwallmandld@gmail.com>
2024-10-02 18:46:14 +0200

    Updated the main file with groups to manage multiple different objects for the game instead of having to call all of them individually

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -15,14 +18,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        # Update the player's state
-        player.update(dt)
+        # Update the objects state
+        for obj in updatable:
+            obj.update(dt)
         
         # Clear the screen
         screen.fill("black")
 
-        # Draw the player
-        player.draw(screen)
+        # Draw the objects
+        for obj in drawable:
+            obj.draw(screen)
 
         # Swap the buffers to display the new frame
         pygame.display.flip()
